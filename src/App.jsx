3f5c433804882253a217f47032ac385805c1c4fe
Yihua Zhang <michaelzhang2@msn.com>
2019-07-09 15:11:58 -0400

    setting up our initial react files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => (
+  <div className='app'>
+    <div className='card'>
+      <h1 className='title'>Our React is working :)</h1>
+    </div>
+  </div>
+);
+
+export default App;
